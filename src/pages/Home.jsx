d2be67d90b24a35b9c962267864d7b730a111a14
shyam369-b <65617308+shyam369-b@users.noreply.github.com>
2024-12-04 11:04:00 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import React from 'react'
+import Hero from '../components/Hero';
+
+function Home() {
+  return (
+    <div className='page-wrapper'>
+        <Navbar />
+        <Hero />
+        
+       
+      <div className='content'>
+        
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
