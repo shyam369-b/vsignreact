bd330cb8cb6a35b250a34308d4e4265bc196b246
shyam369-b <65617308+shyam369-b@users.noreply.github.com>
2024-12-06 22:43:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Contact from './pages/Contactpage'
+
+
 function App() {
   return (
-    <div className="page-wrapper">
-      <Home />
-      
-    
-    </div>
-    
+    <Router>
+      <div className="page-wrapper">
+        <Routes>
+          {/* Define your routes */}
+          <Route path="/" element={<Home />} />
+          
+
+          {/* Redirect example */}
+          <Route path="/pages/Contactpage" element={<Contact />} />
+
+          {/* Catch-all for undefined routes */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
