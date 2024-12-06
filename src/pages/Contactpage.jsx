4836b5df924b89b2b89926f152e972ca2aad7737
shyam369-b <65617308+shyam369-b@users.noreply.github.com>
2024-12-06 19:38:29 +0530

    Updated 0612

---
+++ src/pages/Contactpage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar2 from '../components/Navbar2'
+import Footer from '../components/Footer'
+import Contactdetails from '../components/contactpagecomp/Contactdetails'
+
+function Contactpage() {
+  return (
+    <div className='page-wrapper'>
+        <Navbar2 />
+        <Contactdetails />
+        <div className='content'>
+        
+
+        </div>
+        <Footer />
+      
+    </div>
+  )
+}
+
+export default Contactpage
