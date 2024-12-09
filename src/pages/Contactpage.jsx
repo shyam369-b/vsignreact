a3e32ba7f5e8959e95b4ce42455e9b621ef7b83d
shyam369-b <65617308+shyam369-b@users.noreply.github.com>
2024-12-09 21:51:16 +0530

    Update Contactpage.jsx

--- src/pages/Contactpage.jsx
+++ src/pages/Contactpage.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Navbar2 from '../components/Navbar2'
 import Footer from '../components/Footer'
 import Contactdetails from '../components/contactpagecomp/Contactdetails'
+import Banner from '../components/contactpagecomp/Banner'
 
 function Contactpage() {
   return (
-    <div className='page-wrapper'>
+    <div className='page-wrapper2'>
         <Navbar2 />
+        <Banner />
         <Contactdetails />
-        <div className='content'>
-        
 
-        </div>
         <Footer />
       
     </div>
